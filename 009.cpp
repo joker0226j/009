58124d51a1e25a131008ba4568a072df5ea076b8
joker0226j <48340601+joker0226j@users.noreply.github.com>
2019-04-08 19:33:14 +0800

    Add files via upload

---
+++ 009.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+
+int nume,numf,numtoe;
+
+for(nume=1;nume<=9;num++){
+for(numf=1;numf<=9;numf++){
+printf("%d*%d=%d ",nume,numf,nume*numf);
+}
+printf("\n");
+}
+
+system("PAUSE");
+return 0;
+}
